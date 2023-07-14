fcb43626367d054638375e748e34820d9b14ac45
vilfer <sergey.polin@urfu.me>
2023-07-14 23:56:06 +0300

    update version

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import holidayAPI
 from dotenv import load_dotenv
 import os
 import openai
+import random
 
 load_dotenv()
 openai.api_key = os.environ.get("OPENAI_TOKEN")
@@ -36,19 +37,11 @@ def insert_holiday(holiday, kitty, sparkles):
     return f"""
 Stable Diffusion is an AI art generation model similar to DALLE-2.
 Below is a list of prompts that can be used to generate images with Stable Diffusion:
+- beautiful digital oil vintage greeting card of a Valentine boy and girl by Arthur Hughes
+- beautiful digital oil vintage greeting card steampunk style ballerina key in back by Arthur Hughes
+- greeting card, love, 2 beautiful snow foxes, by tran nguyen, warm colors, cozy
 
-- portait of a homer simpson archer shooting arrow at forest monster, front game card, drark, marvel comics, dark, intricate, highly detailed, smooth, artstation, digital illustration by ruan jia and mandy jurgens and artgerm and wayne barlowe and greg rutkowski and zdislav beksinski
-- pirate, concept art, deep focus, fantasy, intricate, highly detailed, digital painting, artstation, matte, sharp focus, illustration, art by magali villeneuve, chippy, ryan yee, rk post, clint cearley, daniel ljunggren, zoltan boros, gabor szikszai, howard lyon, steve argyle, winona nelson
-- ghost inside a hunted room, art by lois van baarle and loish and ross tran and rossdraws and sam yang and samdoesarts and artgerm, digital art, highly detailed, intricate, sharp focus, Trending on Artstation HQ, deviantart, unreal engine 5, 4K UHD image
-- red dead redemption 2, cinematic view, epic sky, detailed, concept art, low angle, high detail, warm lighting, volumetric, godrays, vivid, beautiful, trending on artstation, by jordan grimmer, huge scene, grass, art greg rutkowski
-- a fantasy style portrait painting of rachel lane / alison brie hybrid in the style of francois boucher oil painting unreal 5 daz. rpg portrait, extremely detailed artgerm greg rutkowski alphonse mucha greg hildebrandt tim hildebrandt
-- athena, greek goddess, claudia black, art by artgerm and greg rutkowski and magali villeneuve, bronze greek armor, owl crown, d & d, fantasy, intricate, portrait, highly detailed, headshot, digital painting, trending on artstation, concept art, sharp focus, illustration
-- closeup portrait shot of a large strong female biomechanic woman in a scenic scifi environment, intricate, elegant, highly detailed, centered, digital painting, artstation, concept art, smooth, sharp focus, warframe, illustration, thomas kinkade, tomasz alen kopera, peter mohrbacher, donato giancola, leyendecker, boris vallejo
-- ultra realistic illustration of steve urkle as the hulk, intricate, elegant, highly detailed, digital painting, artstation, concept art, smooth, sharp focus, illustration, art by artgerm and greg rutkowski and alphonse mucha
-
-I want you to write me a list of detailed prompts exactly about the idea written after IDEA. Follow the structure of the example prompts. This means a very short description of the scene, followed by modifiers divided by commas to alter the mood, style, lighting, and more.
-
-IDEA: cover for celebrating {holiday} picture {kitty}, {sparkles}
+IDEA: greeting card cover for celebrating birthday picture {kitty}, {sparkles}
 
 Give the answer in one Prompt without too much information
 """
@@ -136,17 +129,19 @@ def greeting():
     args = flask.request.args
     response = {}
     if "holiday" in args:
-        # Get the holiday from the arguments
-        holiday = args["holiday"]
+        # get holiday list from holidayAPI
+        holidayList = holidayAPI.getHoliday()
+        # pick random holiday from the list of holidays
+        holiday = holidayList[random.randint(0, len(holidayList)-1)]
         # Generate a completion using OpenAI's text-davinci-003 model
-        prompt = f"Напиши поздравление моему другу, поздравь его с днем {holiday}. Сделай его смешным и запоминающимся."
-        greeting = openai.Completion.create(
+        prompt = f"Write a birthday greeting to my friend, wish him happy birthday and also happy {holiday}. Write a greeting in the format of a poem. Make it funny and memorable."
+        greetingOut = openai.Completion.create(
             model="text-davinci-003",
             prompt=prompt,
             max_tokens=1024,
         )
         # Create a JSON response with the generated greeting
-        response = flask.jsonify(greeting)
+        response = flask.jsonify(greetingOut)
         response.status_code = 200
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response
